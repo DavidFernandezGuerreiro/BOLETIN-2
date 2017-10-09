03089b822b0cea887dc130ebb0de95b09eae5f78
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-10-09 08:48:42 +0200

    Primer Commit. Boletin 2 - 1.

---
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,22 @@
+
+package boletin2_1;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float base = 4F,altura = 3F,area;
+        area=base*altura/2;
+        System.out.println("área ="+area);
+        
+         
+    }
+    
+}
